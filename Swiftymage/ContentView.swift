2eee0fc72a0815e178bf1c066f07704cdbaacac4
Attila Tóth <muchcoffe@gmail.com>
2019-10-28 17:46:52 +0100

    Initial Commit

---
+++ Swiftymage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftymage
+//
+//  Created by Attila Tóth on 2019. 10. 28..
+//  Copyright © 2019. Attila Tóth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftymageTests/SwiftymageTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftymageTests.swift
+//  SwiftymageTests
+//
+//  Created by Attila Tóth on 2019. 10. 28..
+//  Copyright © 2019. Attila Tóth. All rights reserved.
+//
+
+import XCTest
+@testable import Swiftymage
+
+class SwiftymageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
