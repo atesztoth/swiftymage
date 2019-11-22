489b275d60dd5bf07dbae073888edeb722c87ba0
Attila Tóth <muchcoffe@gmail.com>
2019-11-22 14:07:01 +0100

    added base for Main screen, and network fetcher

---
+++ Swiftymage/Main/MainRouter.swift
@@ -0,0 +1,9 @@
+//
+//  MainRouter.swift
+//  Swiftymage
+//
+//  Created by Attila Tóth on 2019. 11. 22..
+//  Copyright © 2019. Attila Tóth. All rights reserved.
+//
+
+import Foundation
--- SwiftymageTests/SwiftymageTests.swift
+++ SwiftymageTests/SwiftymageTests.swift
@@ -19,16 +19,32 @@ class SwiftymageTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testDataFetcherDataResult() {
+        DataFetcher.sharedInstance.fetchData(from: "https://cataas.com/cat") { (data: Data?, error) in
+            XCTAssertNil(error, "Error was not nil. :(")
+            XCTAssertNotNil(data)
+            XCTAssertNotNil(UIImage(data: data!))
         }
     }
+    
+    func testDataFetcherAutoParse() {
+        struct TestType: FetchAutoParsable {
+            static func parse(from: Data) -> TestType? {
+                TestType()
+            }
+        }
+        
+        DataFetcher.sharedInstance.fetchData(from: "https://cataas.com/cat") { (data: TestType?, error) in
+            XCTAssertNil(error, "Error was not nil. :(")
+            XCTAssertNotNil(data!)
+        }
+    }
+
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
